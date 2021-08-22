8d7d08cb7f7f2cf92e01226a40113ca81a9790de
Sennarion <janadine.pro@gmail.com>
2021-08-22 21:18:50 +0300

    Homework 26: completed

--- js/script.js
+++ js/script.js
@@ -1,59 +1,42 @@
+const key = '140256757c07f3e752b265c7569c113f';
+const card = document.querySelector('.card')
 const cardImage = document.querySelector('.card-img-top');
 const cardTitle = document.querySelector('.card-title');
 const cardText = document.querySelectorAll('.card-text');
 const select = document.querySelector('.form-select');
+card.hidden = true;
 
-function newCity(cityName) {
+function addNewCity(cityName) {
     let city = document.createElement('option');
     city.setAttribute('value', cityName);
     city.innerText = cityName;
     select.append(city);
 }
-newCity('New York');
-newCity('Paris');
-newCity('London');
-newCity('Sydney');
 
-select.addEventListener('change', (event) => {
-    const parametrs = new URLSearchParams({
-        access_key: 'd980918584d58c50725729c01653726e',
-        query: event.target.value
-    });
+addNewCity('New York');
+addNewCity('Paris');
+addNewCity('London');
+addNewCity('Sydney');
+addNewCity('Tokyo');
 
-    async function getWeather() {
-        let response = await fetch(`http://api.weatherstack.com/current?${parametrs}`);
-        return response.json();
-    }
+async function fillCard(event) {
+    let data = await getWeather(event);
+    cardImage.src = data.current.weather_icons[0];
+    cardTitle.innerHTML = `${data.current.observation_time} -- ${data.current.weather_descriptions[0]}`;
+    cardText[0].innerHTML = `Temperature: ${data.current.temperature}`;
+    cardText[1].innerHTML = `${data.location.name}, ${data.location.country}`;
+    card.hidden = false;
+}
 
-    async function drawCard() {
-        let data = await getWeather();
-        cardImage.src = data.current.weather_icons[0];
-        cardTitle.innerHTML = `${data.current.observation_time} -- ${data.current.weather_descriptions[0]}`;
-        cardText[0].innerHTML = `Temperature: ${data.current.temperature}`;
-        cardText[1].innerHTML = `${data.location.name}, ${data.location.country}`;
-    }
-    drawCard()
-});
+async function getWeather(event) {
+    let response = await fetch(`http://api.weatherstack.com/current?access_key=${key}&query=${event.target.value}`);
+    return response.json();
+}
 
+function displayCard(event) {
+    if (event.target.value !== "0") {
+        fillCard(event);
+    } else card.hidden = true;
+}
 
-
-// const parametrs = new URLSearchParams({
-//   access_key: 'd980918584d58c50725729c01653726e',
-//   query: 'Kharkiv'
-// });
-
-// async function getWeather() {
-//     let response = await fetch(`http://api.weatherstack.com/current?${parametrs}`);
-//     return response.json();
-// }
-
-// async function drawCard() {
-//     let data = await getWeather();
-//     cardImage.src = data.current.weather_icons[0];
-//     cardTitle.innerHTML = `${data.current.observation_time} -- ${data.current.weather_descriptions[0]}`;
-//     cardText[0].innerHTML = `Temperature: ${data.current.temperature}`;
-//     cardText[1].innerHTML = `${data.location.name}, ${data.location.country}`;
-// }
-
-// drawCard()
-
+select.addEventListener('change', displayCard);
